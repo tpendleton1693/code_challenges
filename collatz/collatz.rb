ab748fd21f084493136d529afa2b9a4c5ec45597
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-10-10 00:53:13 -0700

    Longest Collatz from 1 to 1,000,000 completed

---
+++ collatz/collatz.rb
@@ -0,0 +1,26 @@
+def longest_collatz
+  n = (1..1_000_000).to_a
+  length = 0
+  a = 0
+  ary = []
+  n.each do |x|
+    ary << x
+    while x != 1
+      if x % 2 == 0
+	x = x/2
+	ary << x
+      else
+	x = 3*x+1
+	ary << x
+      end
+      if ary.length > length
+	a = ary
+	length = ary.length
+      end
+    end
+    ary = []
+  end
+  puts "The number from #{n.first} to #{n.last} that produces the longest collatz sequence is #{a[0]}.  Its sequence has a length of #{length}."
+end
+
+longest_collatz

07c59bb850e962038b5a653f04e54d09a8180378
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-10-13 12:59:24 -0700

    Depth search

--- tree_search/search.rb
+++ tree_search/search.rb
@@ -8,11 +8,24 @@ class Tree
 
 end
 
+def depth_search(node, value)
+  if node.payload == value
+    puts "Search returned #{node.payload}" 
+    node
+  else
+    node.children.each do |child|
+      puts "value in child #{child.payload}"
+      result = depth_search(child, value)
+      return result if result.payload == value
+    end
+    node
+  end
+end
 
 # The "Leafs" of a tree, elements that have no children
-fifth_node    = Tree.new(5, [])
+fifth_node = Tree.new(5, [])
 eleventh_node = Tree.new(11, [])
-fourth_node   = Tree.new(4, [])
+fourth_node = Tree.new(4, [])
 
 
 # The "Branches" of the tree
@@ -22,4 +35,5 @@ seventh_node = Tree.new(7, [sixth_node])
 fifth_node = Tree.new(5, [ninth_node])
 
 # The "Trunk" of the tree
-trunk   = Tree.new(2, [seventh_node, fifth_node])
+trunk = Tree.new(2, [seventh_node, fifth_node])
+
---
+++ tree_search/search_test.rb
@@ -0,0 +1,42 @@
+require 'minitest/autorun'
+require_relative 'search'
+
+class TestSearch < MiniTest::Test
+
+  def setup
+
+    # The "Leafs" of a tree, elements that have no children
+    @fifth_node = Tree.new(5, [])
+    @eleventh_node = Tree.new(11, [])
+    @fourth_node = Tree.new(4, [])
+
+
+    # The "Branches" of the tree
+    @ninth_node = Tree.new(9, [@fourth_node])
+    @sixth_node = Tree.new(6, [@fifth_node, @eleventh_node])
+    @seventh_node = Tree.new(7, [@sixth_node])
+    @fifth_node = Tree.new(5, [@ninth_node])
+
+    # The "Trunk" of the tree
+    @trunk = Tree.new(2, [@seventh_node, @fifth_node])
+
+  end
+
+  def test_search_two
+    assert_equal(@trunk, depth_search(@trunk, 2))
+  end
+
+  def test_search_eleven
+    assert_equal(@eleventh_node, depth_search(@trunk, 11))
+  end
+
+  def test_search_four
+    assert_equal(@fourth_node, depth_search(@trunk, 4))
+  end
+
+  def test_search_invalid
+    skip
+    assert_equal(nil, depth_search(@trunk, 577))
+  end
+
+end

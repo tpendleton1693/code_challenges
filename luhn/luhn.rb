4271ad339d1aa25e43bf3f27c6ed1fb6c8bd4fcb
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-10-21 22:23:09 -0700

    Cleaned up luhn algorithm

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,30 +1,25 @@
 module Luhn
+  def self.digits(n, base=10)
+    q, r = n.divmod(base)
+    (q > 0 ? digits(q) : []) + [r]
+  end
+  
   def self.is_valid?(number)
-    number = number.to_s.split('')
-    ary = []
+    number = digits(number)
 
-    number.each_with_index do |n, n_index|
-      n = n.to_i
+    sum = number.map.with_index do |n, n_index|
       if n_index == 0 || n_index % 2 == 0
 	if n + n > 9
-	  n = n + n - 9
-	  ary << n
+	  n + n - 9
 	else
-	  n = n + n
-	  ary << n
+	  n + n
 	end
       else
-	ary << n
+	n
       end
-    end
+    end.inject(:+)
 
-    sum = ary.inject { |x, y| x + y }
-
-    if sum % 10 == 0
-      true
-    else
-      false
-    end
+    return sum % 10 == 0
   end
 end
 
